c6572782e048d694a7617e699dcd37449e6e6099
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-10-25 10:34:52 -0400

    implemented resolving variable and assign Expr

--- neutron/Resolver.java
+++ neutron/Resolver.java
@@ -21,6 +21,37 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        for (Token name : stmt.names) {
+            declare(name);
+        }
+        if (stmt.initializers.size() != 0) {
+            for (Expr initializer : stmt.initializers) {
+                reslove(initializer);
+            }
+        }
+        for (Token name : stmt.names) {
+            define(name);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+        if (!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+            neutron.error(expr.name, "can't read local variable in it's own initializer");
+        }
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
+    // Visiting assignment Expression
+    public Void visitAssignExpr(Expr.Assign expr) {
+        reslove(expr);
+        resolveLocal(expr, expr.name);
+        return null;
+    }
     // -------- Utility Methods ----------------
 
     void reslove(List<Stmt> statements) {
@@ -40,12 +71,35 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         expr.accept(this);
     }
 
+    // Stating a scope
     private void beginScope() {
         scopes.push(new HashMap<String, Boolean>());
     }
 
-    private void endScope(){
+    // Ending a scope
+    private void endScope() {
         scopes.pop();
     }
 
+    // Handling declaration of variables
+    private void declare(Token name) {
+        if (scopes.isEmpty())
+            return;
+        scopes.peek().put(name.lexeme, false);
+    }
+
+    // Handling defination of variable;
+    private void define(Token name) {
+        if (scopes.isEmpty())
+            return;
+        scopes.peek().put(name.lexeme, true);
+    }
+
+    private void resolveLocal(Expr expr, Token name) {
+        for (int i = scopes.size() - 1; i >= 0; i++) {
+            if (scopes.get(i).containsKey(name.lexeme)) {
+                interpreater.resolve(expr, scopes.size() - 1 - i);
+            }
+        }
+    }
 }
